418a657e1f187468fc0e810b449712dcbb491974
brett <brett.j.heroux@gmail.com>
2014-08-02 21:57:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pvs::Application.config.secret_token = '550fd1486055e18546fdc44eb4c8f4f519d26ec40875b9d8b2938a32e43291d6459c7a360c56e00df74aa9a1fae7fa3e0479958ff2f860daf97535c6ef46320a'
